791ea00d4f4f2adb2a78bde81cff219d2e811391
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-25 18:16:39 -0400

    Start doing stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Codec.Archive.Zip
+import Path
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+pathsToJar files = do paths <- map parseRelFile files
+                      classesToJar paths
+                      
+
+--classesToJar :: [ClassFile] -> JarName -> JarArchive
+--classesToJar :: [Path Rel File] -> IO ()
+classesToJar filePaths = jarArchive
+  where jarArchive = map mkEntrySelector filePaths
+--        filePaths  = map parseRelFile fileNames
