4a9bd324b234db52faa4d2e8ecc2ef112518b298
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-25 23:27:37 -0400

    Add working createJar function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,24 @@
 module Lib where
 
 import Codec.Archive.Zip
+import Control.Monad.IO.Class
+import Control.Monad.Catch
 import Path
 import Path.Internal
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- | Creates an empty jar archive at the given relative filepath location.
+-- The name of the archive and itsfile ending should be included in the
+-- filepath.
+--
+-- For example, passing in "src/Main.jar" would create an empty jar archive
+-- named "Main.jar" in the "src" sub-directory of the current directory
+createJar :: (MonadIO m, MonadCatch m) => FilePath -> m ()
+createJar location = createArchive path (return())
+  where path = case parseRelFile location of
+                 Just p -> p
 
 pathsToJar files = do paths <- map parseRelFile files
                       classesToJar paths
@@ -17,6 +30,16 @@ classesToJar filePaths = jarArchive
 --        filePaths  = map parseRelFile fileNames
 --
 
-createJar pathE = createArchive path (return())
-  where path = case pathE of
-                 Just p -> p
+
+{-
+addToJar jar file = do zipActionNoM <- zipAction
+                       withArchive jarPath zipAction
+                       return ()
+  where jarPath  = case parseRelFile jar of
+                     Just p -> p
+        filePath = case parseRelFile file of
+                     Just p -> p
+        fileContents = readFile file
+        zipAction = do fileContentsNoM <- fileContents
+                       addEntry Store fileContentsNoM (mkEntrySelector filePath)
+                       -}
