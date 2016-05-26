f349a1a80e23b30e264e942467ee4f67862cff14
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-25 23:04:04 -0400

    Do some more stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,16 +2,21 @@ module Lib where
 
 import Codec.Archive.Zip
 import Path
+import Path.Internal
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 pathsToJar files = do paths <- map parseRelFile files
                       classesToJar paths
-                      
 
 --classesToJar :: [ClassFile] -> JarName -> JarArchive
 --classesToJar :: [Path Rel File] -> IO ()
 classesToJar filePaths = jarArchive
   where jarArchive = map mkEntrySelector filePaths
 --        filePaths  = map parseRelFile fileNames
+--
+
+createJar pathE = createArchive path (return())
+  where path = case pathE of
+                 Just p -> p
