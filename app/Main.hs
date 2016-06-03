3a8ebfe458b95e3675751f92dc911e8953308b1c
Christopher Wells <cwellsny@nycap.rr.com>
2016-06-03 01:29:39 -0400

    Update main file

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,12 @@
 module Main where
 
-import Codec.Archive.Zip
 import System.Environment (getArgs)
 
 import Lib
 
---main :: IO ()
+main :: IO ()
 main = do
   args <- getArgs
   let jarLocation = head args
-  case createEmptyJar jarLocation of
-    Just x -> x
-{-
-  let x = case createEmptyJar jarLocation of
-            Just action -> action
-  return x
--}
+  createEmptyJar jarLocation
+  return ()
